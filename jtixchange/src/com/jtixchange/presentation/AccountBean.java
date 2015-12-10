6a9d73364eeff3e8e9fcc8b81007ed35cb042673
brian.dworak <brian.dworak@dwgsascara01.cloud-core.aig.net>
2015-12-10 13:20:33 -0600

    Added German

--- jtixchange/src/com/jtixchange/presentation/AccountBean.java
+++ jtixchange/src/com/jtixchange/presentation/AccountBean.java
@@ -46,7 +46,8 @@ public class AccountBean extends BaseBean {
 		final List<String> langList = new ArrayList<String>();
 		langList.add("english");
 		langList.add("japanese");
-	
+		langList.add("german");
+		
 		LANGUAGE_LIST = Collections.unmodifiableList(langList);
 
 		final List<String> catList = new ArrayList<String>();
