d4a97a7fa8ceb057cb3a35cec74126d34966050b
brian.dworak <brian.dworak@dwgsascara01.cloud-core.aig.net>
2016-01-28 15:47:38 -0600

    Changed Texan to American

--- jtixchange/src/com/jtixchange/presentation/AccountBean.java
+++ jtixchange/src/com/jtixchange/presentation/AccountBean.java
@@ -55,7 +55,7 @@ public class AccountBean extends BaseBean {
 		langList.add("english");
 		langList.add("japanese");
 		langList.add("spanish");
-		langList.add("Texan");
+		langList.add("American");
 		
 		
 		
