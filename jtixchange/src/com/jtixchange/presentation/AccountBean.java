1836dedc5c38cfbbbf514c47694878168d6b854a
brian.dworak <brian.dworak@dwgsascara01.cloud-core.aig.net>
2016-05-09 10:54:50 -0500

    Remove German

--- jtixchange/src/com/jtixchange/presentation/AccountBean.java
+++ jtixchange/src/com/jtixchange/presentation/AccountBean.java
@@ -55,7 +55,7 @@ public class AccountBean extends BaseBean {
 		langList.add("English");
 		langList.add("Spanish");
 		langList.add("Italian");
-		langList.add("German");
+		//langList.add("German");
 		//langList.add("Peruvian");
 		//langList.add("Portugese");
 		
