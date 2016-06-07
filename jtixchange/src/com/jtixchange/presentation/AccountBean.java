e5cc3ac107b1f411b7361ac72d8939b2c99869f9
brian.dworak <brian.dworak@dwgsascara01.cloud-core.aig.net>
2016-06-06 20:28:30 -0500

    Added French

--- jtixchange/src/com/jtixchange/presentation/AccountBean.java
+++ jtixchange/src/com/jtixchange/presentation/AccountBean.java
@@ -56,7 +56,7 @@ public class AccountBean extends BaseBean {
 		langList.add("Spanish");
 		langList.add("Italian");
 		langList.add("German");	
-		//langList.add("French");
+		langList.add("French");
 		
 		LANGUAGE_LIST = Collections.unmodifiableList(langList);
 
